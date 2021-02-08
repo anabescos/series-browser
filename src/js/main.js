abc4b90da4d984aff516b42087f989368da00992
anabescos <anabes88@gmail.com>
2021-02-08 18:52:17 +0100

    Add rendering of titles and images

--- src/js/main.js
+++ src/js/main.js
@@ -3,10 +3,12 @@
 const inputElement =document.querySelector ('.js-input');
 const buttonElement = document.querySelector ('.js-button'); 
 const inputValue = inputElement.value
+const ulSeriesList = document.querySelector('.js-seriesList');
 let seriesList=[];
 
 // Get series from API
-function getDataFromApi() {
+function getDataFromApi(ev) {
+    ev.preventDefault();
     fetch (`http://api.tvmaze.com/search/shows?q=${inputValue}`)
     .then (response => response.json())
     .then (data => {
@@ -14,10 +16,36 @@ function getDataFromApi() {
             
             const series = data[index].show;
             seriesList.push(series);
+            
         }
+        renderSeries();
     });
     console.log(seriesList);
     
 }
 
 buttonElement.addEventListener('click', getDataFromApi);
+
+// paint series list
+
+
+function renderSeries() {
+    const defaultImg = "../assets/images/download (1).png";
+    let htmlCode="";
+
+    for (const eachItem of seriesList) {
+        htmlCode += "<li class='js-series'>";
+        htmlCode += `<h2 class='js-seriesTitle''>${eachItem.name}</h2>`;  
+        const seriesImg = eachItem.image;
+        console.log(seriesImg);
+        if (seriesImg === null) {
+            htmlCode +=`<img src="${defaultImg}">`
+        } else {
+            htmlCode += `<img src=${seriesImg.medium}>`
+        }
+    }
+
+    htmlCode += "</li>";
+    ulSeriesList.innerHTML= htmlCode;
+}
+
