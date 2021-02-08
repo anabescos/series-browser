485d5e0b5984f4bc4b58072cab6122fc7a1817b0
anabescos <anabes88@gmail.com>
2021-02-08 17:00:02 +0100

    Add fetch to server

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,17 @@
 'use strict';
 
-console.log('>> Ready :)');
+const inputElement =document.querySelector ('.js-input');
+const buttonElement = document.querySelector ('.js-button'); 
+
+let series=[];
+
+fetch ('http://api.tvmaze.com/search/shows?q=girls')
+.then (response => response.json())
+.then (data => {
+    for (let index = 0; index < data.length; index++) {
+        
+        series=data[index].show;
+        console.log(series);
+        
+    }
+});
