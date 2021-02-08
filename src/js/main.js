a0faeaa81ebfaa0319c926a5c21862954128d317
anabescos <anabes88@gmail.com>
2021-02-08 17:15:21 +0100

    Clean code and add event

--- src/js/main.js
+++ src/js/main.js
@@ -2,16 +2,20 @@
 
 const inputElement =document.querySelector ('.js-input');
 const buttonElement = document.querySelector ('.js-button'); 
-
+const inputValue = inputElement.value
 let series=[];
 
-fetch ('http://api.tvmaze.com/search/shows?q=girls')
-.then (response => response.json())
-.then (data => {
-    for (let index = 0; index < data.length; index++) {
-        
-        series=data[index].show;
-        console.log(series);
-        
-    }
-});
+// Get series from API
+function getDataFromApi() {
+    fetch (`http://api.tvmaze.com/search/shows?q=${inputValue}`)
+    .then (response => response.json())
+    .then (data => {
+        for (let index = 0; index < data.length; index++) {
+            
+            series = data[index].show;
+        }
+    });
+    
+}
+
+buttonElement.addEventListener('click', getDataFromApi);
